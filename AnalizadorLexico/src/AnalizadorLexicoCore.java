c607ea3cacf2ec1e845b6d1cc8ff308190551491
Bryshot <bryan9807@gmail.com>
2020-02-13 20:24:28 +0100

    Update AnalizadorLexicoCore.java

--- AnalizadorLexico/src/AnalizadorLexicoCore.java
+++ AnalizadorLexico/src/AnalizadorLexicoCore.java
@@ -4,10 +4,9 @@ import java.io.Reader;
 public class AnalizadorLexicoCore {
 
 	enum Estado {
-		INICIO, REC_POR, REC_DIV, REC_PAP, REC_PCIERR, REC_COMA, REC_IGUAL,
-		REC_MAS, REC_MENOS, REC_ID, REC_ENT, REC_0, REC_IDEC, REC_DEC,
-		REC_COM,REC_EOF
-		}
+		INICIO, REC_POR, REC_DIV, REC_PAP, REC_PCIERR, REC_PCOMA, REC_IGUAL,REC_LT,REC_LE,REC_GT, REC_GE,REC_NE,REC_ASIG,REC_EX,
+		REC_MAS, REC_MENOS, REC_NOMBRE, REC_ENT, REC_0,
+		}// REC_IDEC, REC_DEC,REC_COM,REC_EOF,
 	
 	private Reader input; // flujo de entrada
 	private StringBuffer lex; // cadena para almacenar el lexema de la u. léxica
@@ -27,7 +26,7 @@ public class AnalizadorLexicoCore {
 		columnaActual=1;
 	}
 	
-	/*public UnidadLexica sigToken() throws IOException 
+	public UnidadLexica sigToken() throws IOException 
 	{
 		// Fase de inicialización
 		estado = Estado.INICIO;
@@ -44,33 +43,49 @@ public class AnalizadorLexicoCore {
 				else if (hayDigitoPos()) transita(Estado.REC_ENT);
 				else if (hayCero()) transita(Estado.REC_0);
 				else if (haySuma()) transita(Estado.REC_MAS);
-		else if (hayAlmohadilla()) transitaIgnorando(Estado.REC_COM);
-		else if (haySep()) transitaIgnorando(Estado.INICIO);
-		else if (hayEOF()) transita(Estado.REC_EOF);
-		else error();
-		break;
-		case REC_ID:
-		if (hayLetra() || hayDigito()) transita(Estado.REC_ID);
-		else return unidadId();
-		break;
-		case REC_ENT:
-		if (hayDigito()) transita(Estado.REC_ENT);
-		else if (hayPunto()) transita(Estado.REC_IDEC);
-		else return unidadEnt();
-		break;
-		case REC_0:
-		if (hayPunto()) transita(Estado.REC_IDEC);
-		else return unidadEnt();
-		break;
-		case REC_MAS:
-		if (hayDigitoPos()) transita(Estado.REC_ENT);
-		else if(hayCero()) transita(Estado.REC_0);
-		else return unidadMas();
-		break;
-		...
-		case REC_POR: return unidadPor();
-		...
+				else if (hayAlmohadilla()) transitaIgnorando(Estado.REC_COM);
+				else if (haySep()) transitaIgnorando(Estado.INICIO);
+				else if (hayEOF()) transita(Estado.REC_EOF);
+				else error();
+				break;
+			case REC_ID:
+				if (hayLetra() || hayDigito()) transita(Estado.REC_ID);
+				else return unidadId();
+				break;
+			case REC_ENT:
+				if (hayDigito()) transita(Estado.REC_ENT);
+				else if (hayPunto()) transita(Estado.REC_IDEC);
+				else return unidadEnt();
+				break;
+			case REC_0:
+				if (hayPunto()) transita(Estado.REC_IDEC);
+				else return unidadEnt();
+				break;
+			case REC_MAS:
+				if (hayDigitoPos()) transita(Estado.REC_ENT);
+				else if(hayCero()) transita(Estado.REC_0);
+				else return unidadMas();
+				break;
+			case REC_POR: return unidadPor();
+			}
 		}
-		}
-		}*/
+	}
+	private boolean hayLetra() {return this.sigCar >= 'a' && this.sigCar <= 'z' || this.sigCar >= 'A' && this.sigCar <= 'z';}
+	private boolean hayCero(){return this.sigCar == '0' ;}
+	private boolean hayDigitoPos(){return this.sigCar >= '1'&& this.sigCar <= '9' ;}
+	private boolean hayPunto(){return this.sigCar == '.' ;}
+	private boolean haySeparador(){return this.sigCar == '&' ;}
+	private boolean hayPComa(){return this.sigCar == ';' ;}
+	private boolean hayPApertura(){return this.sigCar == ')' ;}
+	private boolean hayPCierre(){return this.sigCar == ')' ;}
+	private boolean hayMenor(){return this.sigCar == '<';}
+	private boolean hayMayor(){return this.sigCar == '>' ;}
+	private boolean hayIgual(){return this.sigCar == '=' ;}
+	private boolean hayEx(){return this.sigCar == '!' ;}
+	private boolean hayDiv(){return this.sigCar == '/' ;}
+	private boolean hayMas(){return this.sigCar == '+' ;}
+	private boolean hayMenos(){return this.sigCar == '-' ;}
+	private boolean hayPor(){return this.sigCar == '*' ;}
+	private void transita(Estado newState) {this.estado = newState;}
+	private UnidadLexica error() {return null;}
 }
